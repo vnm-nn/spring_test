5520c8f973cb4519cd1c9b613f83e2275f56baf8
vnm-nn <mail.klmnv@gmail.com>
2022-04-16 12:55:14 +0300

    123

---
+++ src/main/java/com/spring/mvc_hiber/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.spring.mvc_hiber.dao;
+
+import com.spring.mvc_hiber.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/spring/mvc_hiber/entity/Employee.java
@@ -0,0 +1,38 @@
+package com.spring.mvc_hiber.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
